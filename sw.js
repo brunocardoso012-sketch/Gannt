d29aadffb42ed096067e37f33cb3237696e18ffb
brunocardoso012-sketch <brunocardoso.012@gmail.com>
2026-04-10 20:43:47 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'gantt-pwa-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// Instala o Service Worker e guarda os arquivos no cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Responde às requisições da rede (permite abrir mais rápido)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
